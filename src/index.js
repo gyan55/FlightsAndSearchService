14e51d20903db81f5084245020dbeb9885937fa3
gyanabritish <gyannayak396@gmail.com>
2023-01-09 19:02:52 +0530

    server config using env

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 
+const {PORT} = require('./config/serverConfig');
+
 const setupAndStartServer = async() =>{
 // create the express  object
 
 const app = express();
-const PORT = 3000;
+
 
 app.listen(3000, () => {
    console.log(`server started at ${PORT}`);
+   
 })
 
+
 }
+
+setupAndStartServer();
