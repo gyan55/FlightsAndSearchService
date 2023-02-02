a4781f7ab696fde62051f4997bc5ec087315703e
gyanabritish <gyannayak396@gmail.com>
2023-02-02 19:54:11 +0530

    added airplanes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const {PORT} = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 const CityRepository = require('./repository/city-repository');
+const db = require('./models');
 
 const setupAndStartServer = async() =>{
 // create the express  object
@@ -16,10 +17,15 @@ app.use(bodyParser.urlencoded({extended : true}));
 
 app.use('/api',ApiRoutes); 
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
    console.log(`server started at ${PORT}`);
    const repo = new CityRepository();
-}) 
+
+   if(process.env.SYNC_DB){
+       db.sequelize.sync({alter:true});
+   }
+  
+}); 
 
 
 }
